dfa78c9f6236d316781f6d714aeb04999465d8bb
RahulKMa <rahulmanoj167@gmail.com>
2025-12-31 14:34:19 +0530

    Cleanup: Remove deprecated backend files and sync modifications

--- ml_pipeline/utils/config.py
+++ ml_pipeline/utils/config.py
@@ -1,3 +1,3 @@
-WINDOW_SIZE = 50      # timesteps per window
-STRIDE = 10           # overlap
+WINDOW_SIZE = 50      # means the AI will look at 50 consecutive time steps at once.
+STRIDE = 10           # overlap #After looking at steps 1 through 50, the AI jumps forward 10 steps and looks at steps 11 through 60.
 DATA_PATH = "../data/simulation_data.csv"
